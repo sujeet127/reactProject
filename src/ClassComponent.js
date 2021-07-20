9142222a17499849eec7386a3918eca82b2d3ba3
sujeet127 <sujeet.kumar6921@gmail.com>
2021-07-21 00:03:50 +0530

    commit

---
+++ src/ClassComponent.js
@@ -0,0 +1,13 @@
+import react ,{Component} from 'react'
+class ClassComponent extends Component{
+
+    render()
+    {
+        return <>
+                    <h1>This is from class  Component</h1>
+                    <h3>Created by Sujeet</h3>
+                 </>
+    }
+
+}
+export default ClassComponent;
---
+++ src/FunctionalComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FunctionalComponent(){
+return <><h1>This is from Functional Component</h1>
+        <h3>Created by Sujeet</h3>
+        </>
+
+}
+export default FunctionalComponent;
